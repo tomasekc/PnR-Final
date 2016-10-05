0df372f5f86dd881b4d299ab03fc89c916345fa2
tomasekc <tomasekc@gilmour.org>
2016-10-05 08:57:04 -0400

    Started student dance

--- student.py
+++ student.py
@@ -52,7 +52,7 @@ class GoPiggy(pigo.Pigo):
             servo(20)
             self.encB(5)
             self.encR(2)
-            self.encl(2)
+            self.encL(2)
             self.encF(5)
             servo(120)
             time.sleep(.1)
